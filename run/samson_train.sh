c0fdac4588e9d23c3d7e50bd111dcf40df984b7e
dv-fenix <45421556+dv-fenix@users.noreply.github.com>
2021-09-26 18:35:03 +0530

    Create Shell Sripts

---
+++ run/samson_train.sh
@@ -0,0 +1,13 @@
+python -u ../src/train.py\
+	-src_dir ../data/Samson/\
+	-num_bands 156\
+	-end_members 3\
+	-encoder_type shallow\
+	-soft_threshold SReLU\
+	-activation Leaky-ReLU\
+	-batch_size 20\
+	-learning_rate 1e-3\
+	-epochs 300\
+	-gaussian_dropout 0.11\
+	-threshold 1.0\
+	-objective SAD >> ../logs/training_out.txt
