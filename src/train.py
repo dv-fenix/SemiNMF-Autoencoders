c29c2bd2d98e98c0d31b7897126aa19199f91dc8
Divyam Goel <divyamgoel3@gmail.com>
2022-03-28 17:17:42 +0530

    Added synthetic experiments

--- src/train.py
+++ src/train.py
@@ -17,7 +17,7 @@ import utils.opts as opts
 # ------------------ Training -------------------- #
 # Load Data
 def train(opt):
-    train_dataloader, test_set = get_dataloader(BATCH_SIZE=opt.batch_size, DIR=opt.src_dir)
+    train_dataloader, test_set = get_dataloader(BATCH_SIZE=opt.batch_size, syn=False, DIR=opt.src_dir)
 
     max_batches = len(train_dataloader)
 
@@ -77,16 +77,16 @@ def train(opt):
                   
                   e, y = model(img.float())
                   ab = e.detach().squeeze().T[i].reshape(95, 95).float()
-                  ab_y = torch.tensor(test_set.labels.T[0]).float()
-                  ab_y1 = torch.tensor(test_set.labels.T[1]).float()
-                  ab_y2 = torch.tensor(test_set.labels.T[2]).float()
-                  error1 =err(ab, ab_y)
+                  ab_y = torch.tensor(test_set.labels.T[0]).reshape(95, 95).float()
+                  ab_y1 = torch.tensor(test_set.labels.T[1]).reshape(95, 95).float()
+                  ab_y2 = torch.tensor(test_set.labels.T[2]).reshape(95, 95).float()
+                  error1 = err(ab, ab_y)
                   error1 = torch.sum(error1).float()
 
-                  error2 =err(ab, ab_y1)
+                  error2 = err(ab, ab_y1)
                   error2 = torch.sum(error2).float()
 
-                  error3 =err(ab, ab_y2)
+                  error3 = err(ab, ab_y2)
                   error3 = torch.sum(error3).float()
 
                   error_ = [error1.item(), error2.item(), error3.item()]
--- src/train_utils.py
+++ src/train_utils.py
@@ -67,14 +67,50 @@ class Samson(data.Dataset):
         return os.path.exists(os.path.join(self.root, self.img_folder, self.training_file)) and os.path.exists(
             os.path.join(self.root, self.gt_folder, self.labels_file)
         )
+
+
+class Synthetic(data.Dataset):
+    def __init__(self, A_shape, S_shape):
+        super(Synthetic, self).__init__()
+        # Rademacher Distribution
+        self.S = 2 * torch.randint(0, 2, S_shape) - 1
+        #self.S = 0.1 * self.S
+        #self.A = torch.randn(A_shape)
+        input = torch.rand(A_shape)
+        input[input<0] = 0
+        self.A = input/torch.sum(input, dim=0)
+        self.train_data = torch.matmul(self.A.float(), self.S.float()) + (0.1**0.5)*torch.randn(2500, 200)
+
+    def __getitem__(self, index):
+        """Get images and target for data loader.
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target) where target is index of the target class.
+        """
         
+        img = self.train_data[index]
+
+        return img
+
+    def __len__(self):
+        """Return size of dataset."""
+
+        return len(self.train_data)
+
         
-def get_dataloader(BATCH_SIZE: int, DIR):
-    """Create a DataLoader for input data stream."""
+def get_dataloader(BATCH_SIZE: int, syn: bool, DIR):
+    
     trans = tvtf.Compose([tvtf.ToTensor()])
 
     # Load train data
-    source_domain = Samson(root=DIR, transform=trans, target_transform=trans)
-    source_dataloader = torch.utils.data.DataLoader(source_domain, BATCH_SIZE)
+    if syn:
+        source_domain = Synthetic((2500, 5), (5, 200))
+        source_dataloader = torch.utils.data.DataLoader(source_domain, BATCH_SIZE,
+                                                        worker_init_fn=seed_worker(41))
+    else:    
+        source_domain = Samson(root=DIR, transform=trans, target_transform=trans)
+        source_dataloader = torch.utils.data.DataLoader(source_domain, BATCH_SIZE,
+                                                        worker_init_fn=seed_worker(41))
     
     return source_dataloader, source_domain
